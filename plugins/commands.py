b912ceb92a9413af6134471eb5c9124ae56e7075
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2022-09-24 14:51:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,8 +47,8 @@ async def start(client, message):
             InlineKeyboardButton('🔍 Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🕵 Go Inline', switch_inline_query='')
             ],[
-            InlineKeyboardButton('⭕ Support ⭕', url='https://t.me/Knmlpro2update'),
-            InlineKeyboardButton('⭕ Updates ⭕', url='https://t.me/Knmlpro2support_bot')
+            InlineKeyboardButton('⭕ Updates ⭕', url='https://t.me/Knmlpro2update'),
+            InlineKeyboardButton('⭕ Support ⭕', url='https://t.me/Knmlpro2support_bot')
             ],[
             InlineKeyboardButton('🆘 Help', callback_data='help'),
             InlineKeyboardButton('👥 About', callback_data='about')
@@ -98,8 +98,8 @@ async def start(client, message):
             InlineKeyboardButton('🔍 Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🕵 Go Inline', switch_inline_query='')
             ],[
-            InlineKeyboardButton('⭕ Support ⭕', url='https://t.me/Knmlpro2update'),
-            InlineKeyboardButton('⭕ Updates ⭕', url='https://t.me/Knmlpro2support_bot')
+            InlineKeyboardButton('⭕ Updates ⭕', url='https://t.me/Knmlpro2update'),
+            InlineKeyboardButton('⭕ Support ⭕', url='https://t.me/Knmlpro2support_bot')
             ],[
             InlineKeyboardButton('🆘 Help', callback_data='help'),
             InlineKeyboardButton('👥 About', callback_data='about')
