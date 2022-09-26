ae396822d8ded8caad654c52f5aa1cdc42e7961b
nandanreigns <113355587+nandanreigns@users.noreply.github.com>
2022-09-26 19:08:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Knmlpro2>𝚁𝙰𝙰𝙳𝙷𝙰 𝙺𝚁𝙸𝚂𝙷𝙽𝙰</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/hakthiniwaitmadu>𝙰𝙽𝙶𝙰𝚈𝙰𝚁𝙺𝙰𝙽𝙽𝙸</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -12,16 +12,16 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Raadha krishnan bot is a open source project. 
-- Source - <a href=https://t.me/Knmlpro2>Click Me😀</a>
+- Source - <a href=https://t.me/hakthiniwaitmadu>Click Me😀</a>
 
 <b>DEVS:</b>
-- <a href=https://t.me/Knmlpro2>Knmlpro2</a>"""
+- <a href=https://t.me/hakthiniwaitmadu>HAKTHINIWAITMADU</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and raadha krishna will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Angayarkanni will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. Raadha krishna should have admin privillage.
+1. Angayarkanni should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,15 +32,16 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- RAADHA KRISHNA Supports both url and alert inline buttons.
+𝙰𝙽𝙶𝙰𝚈𝙰𝚁𝙺𝙰𝙽𝙽𝙸 Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Raadha krishna supports buttons with any telegram media type.
+2. Angayarkanni
+supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/Knmlpro2Filter_Bot)</code>
+<code>[Button Text](buttonurl:http://t.me/Nandukanni_bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
