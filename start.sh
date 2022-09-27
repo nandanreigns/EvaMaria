2ac63630fb381517aadaab505b2c8e801c820a4d
nandanreigns <113355587+nandanreigns@users.noreply.github.com>
2022-09-27 17:45:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/nandanreigns/EvaMaria.git /hakthiniwaitmadu
+  git clone https://github.com/nandanreigns/EvaMaria.git /Knmlpro2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /hakthiniwaitmadu
+  git clone $UPSTREAM_REPO /Knmlpro2
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
